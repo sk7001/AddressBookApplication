396812441f79cf620f49fcd00182b538e5887857
Srinivas Sobhit Kintali <sk7001@srmist.edu.in>
2025-05-23 10:35:28 +0530

    UC5: Added Lombok dependency and used @Data, @NoArgsConstructor, @AllArgsConstructor in DTO/model

--- src/main/java/com/addressBookApp/dto/AddressBookDTO.java
+++ src/main/java/com/addressBookApp/dto/AddressBookDTO.java
@@ -1,18 +1,13 @@
 package com.addressBookApp.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressBookDTO {
     private String name;
     private String phone;
-
-    public AddressBookDTO() {}
-
-    public AddressBookDTO(String name, String phone) {
-        this.name = name;
-        this.phone = phone;
-    }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
 }
